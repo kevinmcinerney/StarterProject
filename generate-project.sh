ae7b8a3012b5b0d85b83deeb323d535a83e9354c
kevin mc inerney <kevinmcinerney8@gmail.com>
2018-09-29 21:34:57 +0100

    fix line

--- generate-project.sh
+++ generate-project.sh
@@ -3,15 +3,12 @@
 project_name=${PWD##*/}
 
 echo Updating Anaconda...
-echo 
 conda update -y -n base conda >/dev/null
 
 echo Creating Virtual Environment...
-echo 
 conda create -y --name $project_name python=3.6 >/dev/null
 
 echo Activating Virtual Environment...
-echo 
 source activate $project_name >/dev/null
 
 echo Adding project name to yml file...
@@ -20,22 +17,17 @@ A="name: "
 A+=$project_name
 
 echo "$A" | cat - environment.yml > temp && mv temp environment.yml # replace first line
-echo 
 
 echo Installing Libraries in Virtual Environment
-echo 
 conda env update --file environment.yml >/dev/null
 
 echo Cloning xutils...
-echo 
 git clone https://github.com/kevinmcinerney/xutils.git
 
 echo Installing xutils...
-echo 
 cd xutils
 pip install . >dev/null
 
 echo Entering virtual environment...
-echo 
-subprocess.run('source activate '$project_name, shell=True) >dev/null
+subprocess.run('source activate "project_name', shell=True) >dev/null
 
