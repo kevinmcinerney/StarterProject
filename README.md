f8b220da9f23252030002ac73a58a2012a5d864b
kevin mc inerney <kevinmcinerney8@gmail.com>
2018-09-29 14:38:23 +0100

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# StarterProject
+# To start a new project
 
-#1 To initiliaze a new project run:
-#	>> ./generate-project.sh
+#1 Go to https://github.com/kevinmcinerney/StarterProject and fork the StarterProject
+
+#2 Rename the fork to your new project name
+
+#3 On your local machine, git clone <the url of your new project>
+
+#4 cd into your project and execute the generate-project.sh script
+
+
--- generate-project.sh
+++ generate-project.sh
@@ -3,13 +3,13 @@
 project_name=${PWD##*/}
 
 echo Updating Anaconda
-conda update -y -n base conda
+conda update -y -n base conda >/dev/null
 
 echo Creating Virtual Environment...
-conda create -y --name $project_name python=3.6
+conda create -y --name $project_name python=3.6 >/dev/null
 
 echo Activating Virtual Environment...
-source activate $project_name
+source activate $project_name >/dev/null
 
 echo Adding project name to yml file
 echo "$(tail -n +2 environment.yml)" > environment.yml #remove first line of yml file
@@ -19,6 +19,7 @@ A+=$project_name
 echo "$A" | cat - environment.yml > temp && mv temp environment.yml # replace first line
 
 echo Installing Libraries in Virtual Environment
-conda env update --file environment.yml
+conda env update --file environment.yml >/dev/null
 
+echo To start, run "source activate "$project_name
 
